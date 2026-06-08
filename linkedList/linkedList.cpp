784eb920ce0c4cdb73e5e15f0158fbb5367699bc
Mr Graviton <mr.gopal.gdev@gmail.com>
2026-06-08 13:08:51 +0530

    implement linked list with push front and print functionality

---
+++ linkedList/linkedList.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+      data = value;
+    }
+};
+
+class List {
+  Node* head;
+  Node* tail;
+
+  public: 
+    List() {
+      head = tail = NULL;
+    }
+
+    // Push Front
+    void pushFront(int val) {
+      Node* newNode = new Node(val);
+      if (head == NULL) {
+        head = tail = newNode;
+      } else {
+        newNode->next = head;
+        head = newNode;
+      }
+    }
+
+    // Show list
+    void print() {
+      Node* temp = head;
+      while (temp != NULL) {
+        cout<<temp->data<<" "<<"-> ";
+        temp = temp->next;
+      }
+      cout<<"NULL"<<endl;
+    }
+};
+
+int main() {
+  List l;
+  l.pushFront(1);
+  l.pushFront(2);
+  l.pushFront(3);
+  l.pushFront(4);
+  l.pushFront(5);
+
+  l.print();
+  return 0;
+}
