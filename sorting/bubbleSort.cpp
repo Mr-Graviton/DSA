377eccf6fb0109fc8ddc90154a37b2a1928357c6
Mr-Graviton <mr.gopal.gdev@gmail.com>
2026-08-18 22:51:08 +0530

    bubble sort algorithm

---
+++ sorting/bubbleSort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Time complexity O(n2)
+vector<int> bubbleSort(vector<int> arr) {
+  int n = arr.size();
+  bool isSwap = false;
+  for(int i=0; i<n; i++) {
+    for(int j=0; j<n-i-1; j++) {
+      if (arr[j]>arr[j+1]) { 
+        swap(arr[j], arr[j+1]);
+        isSwap = true;
+      }
+    }
+    if (!isSwap) return arr; // optimization
+  }
+
+  return arr;
+}
+
+int main() {
+  vector<int> arr = {4, 1, 5, 2, 3};
+  vector<int> res = bubbleSort(arr);
+  for(int i=0; i<res.size(); i++) {
+    cout<<res[i]<<" ";
+  }
+  return 0;
+}
